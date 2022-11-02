88f14d3e64878fdd14302ccfe58daf995b762d9a
unknown <joseph.jermaine@yahoo.com>
2022-11-01 22:52:03 -0400

    something

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from '../App';
+import reportWebVitals from '../reportWebVitals';
 import { StateProvider } from './components/StateProvider';
 import reducer, { initialState } from './reducer';
 
